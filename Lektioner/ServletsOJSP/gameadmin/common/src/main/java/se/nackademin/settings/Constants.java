76d47c1910821bcca70fa3bb11bdf3094b7e3e43
Carl Sundvisson <carltham@hotmail.co.uk>
2017-04-09 11:09:56 +0200

    Running version of Lektion 2

--- Lektioner/ServletsOJSP/gameadmin/common/src/main/java/se/nackademin/settings/Constants.java
+++ Lektioner/ServletsOJSP/gameadmin/common/src/main/java/se/nackademin/settings/Constants.java
@@ -4,5 +4,5 @@ package se.nackademin.settings;
  * Created by carl on 04/04/17.
  */
 public class Constants {
-    public static final Long _TIME_TO_LIVE_IN_MILLISECONDS = 600000L;
+    public static final Long _TIME_TO_LIVE_IN_MILLISECONDS = 30000L;
 }
--- Lektioner/ServletsOJSP/gameadmin/login/src/main/java/se/nackademin/servlets/LoginServlet.java
+++ Lektioner/ServletsOJSP/gameadmin/login/src/main/java/se/nackademin/servlets/LoginServlet.java
@@ -2,6 +2,7 @@
  * Created by carl on 04/04/17.
  */
 package se.nackademin.servlets;
+
 import se.nackademin.backend.FakeDB;
 import se.nackademin.domain.User;
 
@@ -13,44 +14,66 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class LoginServlet extends HttpServlet
-{
+public class LoginServlet extends HttpServlet {
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException
-    {
+            throws ServletException, IOException {
         response.setContentType("text/html");
-        String username=request.getParameter("username");
-        String password=request.getParameter("password");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
         PrintWriter writer = response.getWriter();
         writer.println("Från GET :");
-        String message="Username is : "+ username + "<br/> Password is :" + password ;
+        String message = "Username is : " + username + "<br/> Password is :" + password;
         writer.println(message);
     }
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException
-    {
-        HttpSession session = request.getSession();
+            throws ServletException, IOException {
+
+        handlePOST(request, response);
+    }
+
+    private void handlePOST(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+// Prepare any variables to use in this function
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+// prepare headers
         response.setContentType("text/html");
-        String username=request.getParameter("username");
-        String password=request.getParameter("password");
-        User user = (User) session.getAttribute(username);
+
         PrintWriter writer = response.getWriter();
-        if (user!=null && !user.isSessionTimedOut()){
-            writer.println("<p style=\"color:red;\">Du är redan inloggad !!!</p>");
-            return;
+
+        // Check if the session already is active
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute(username);
+        if (user != null && !user.isSessionTimedOut()) {
+            writer.println(
+                    "<p style=\"color:red;\">Du är redan inloggad som : "
+                    + username + "!!!</p>");
+        } else {
+// Othervise check login details
+            user = FakeDB.login(username, password);
+            if (user != null && user.getPassword().equals(password)) {
+                writer.println("<p style=\"color:blue;\">Välkommen du är nu inloggad :<br/>");
+                writer.println("Användarnamn : " + username + "</p>");
+                session.setAttribute(username, user);
+            } else {
+                writer.println("<p style=\"color:red;\">Fel användarnamn / lösenord</p>");
+            }
         }
 
-        user = FakeDB.login(username, password);
-        if (user!=null&& user.getPassword().equals(password)){
-            writer.println("Välkommen du är nu inloggad :");
-            writer.println("Användarnamn : "+ username + "<br/>");
-            session.setAttribute(username, user);
-        }else {
-            writer.println("Fel användarnamn / lösenord");
-        }
-        // response.sendRedirect("/loggedin");
+// In any case present an option to orientatte out from the display page.
+// Here we just go to the previous page
+        writer.println("<p><button onclick=\"goBack()\">Go Back</button>\n"
+                + "\n"
+                + "<script>\n"
+                + "function goBack() {\n"
+                + "    window.history.back();\n"
+                + "}\n"
+                + "</script></p>");
+// response.sendRedirect("/loggedin"); 
     }
 }
